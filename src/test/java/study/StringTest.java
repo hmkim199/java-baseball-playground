172edbcaaa988fbce6acb0cb0dd35985b808ca14
hmkim199 <hmkim199@gmail.com>
2023-10-29 16:52:39 +0900

    test: string charAt test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -28,4 +29,29 @@ public class StringTest {
         String actual = "(1,2)".substring(1,4);
         assertThat(actual).isEqualTo("1,2");
     }
+
+    @Test
+    void charAt(){
+        Character actual = "123".charAt(2);
+        assertThat(actual).isEqualTo('3');
+    }
+
+    @Test
+    @DisplayName("인덱스 예외 - 예외 던진 주체 테스트")
+    void charAtExceptionThrownBy(){
+        assertThatThrownBy(() -> {
+            Character actual = "12".charAt(2);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+          .hasMessageContaining("String index out of range: 2");
+    }
+
+    @Test
+    @DisplayName("인덱스 예외 - 타입 테스트")
+    void charAtExceptionOfType(){
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    Character actual = "ab".charAt(2);
+                }).withMessageMatching("String index out of range: 2");
+    }
+
 }
